42bc4f2729bcb66a7f49a95564f659c6d9063845
Walther H Diechmann <walther@diechmann.net>
2022-05-31 10:55:02 +0200

    refactor menus

--- app/components/navigation/menu_component.rb
+++ app/components/navigation/menu_component.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 class Navigation::MenuComponent < ViewComponent::Base
-  def initialize(nav_classes:, link_classes:, current:)
+  include Turbo::StreamsHelper
+  include Turbo::FramesHelper
+
+  def initialize(current:, nav_classes:, link_classes:)
+    @id = current.account.id rescue 0
     @current = current
     @nav_classes = nav_classes
     @link_classes = link_classes
-    @id = 'mobile'
   end
 
 end
