08eb300d3d1959f8491f22fc3b6bcabf11ad7c19
Walther H Diechmann <walther@diechmann.net>
2022-07-06 15:56:45 +0200

    regression on participant service

--- app/services/participant_service.rb
+++ app/services/participant_service.rb
@@ -1,15 +1,15 @@
 class ParticipantService < AbstractResourceService
-  def create( resource )
+  def create( resource, resource_class )
     result = super(resource)
-    if result.status == :not_valid 
+    if result.status != :created 
       resource.participantable = resource_class.new if resource.participantable.nil?
     end
     result
   end
 
-  def update resource, resource_params
+  def update( resource, resource_params, resource_class )
     result = super(resource,resource_params)
-    if result.status == :not_valid 
+    if result.status != :updated
       resource.participantable = resource_class.new if resource.participantable.nil?
     end
     result
