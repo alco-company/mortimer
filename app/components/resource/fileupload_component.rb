73efdceab5c23dfc5dc2c7c58b671108a34c440c
Walther H Diechmann <walther@diechmann.net>
2022-07-06 15:53:02 +0200

    fix fileupload missing thumb

--- app/components/resource/fileupload_component.rb
+++ app/components/resource/fileupload_component.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class Resource::FileuploadComponent < ViewComponent::Base
-  def initialize(resource:, form:, field:, size: "40x40!")    
-    @resource= resource 
+  def initialize(url:, form:, field:)    
+    @url= url 
     @form= form 
     @field= field
-    @size= size
   end
   
 
