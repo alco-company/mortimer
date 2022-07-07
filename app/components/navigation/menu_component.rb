dddaf7b38170816b80907b91d40bf56e5459d000
Walther H Diechmann <walther@diechmann.net>
2022-07-07 10:37:43 +0200

    make impersonate render menu

--- app/components/navigation/menu_component.rb
+++ app/components/navigation/menu_component.rb
@@ -4,8 +4,9 @@ class Navigation::MenuComponent < ViewComponent::Base
   include Turbo::StreamsHelper
   include Turbo::FramesHelper
 
-  def initialize(current:, nav_classes:, link_classes:)
+  def initialize( destination:, current:, nav_classes:, link_classes: )
     @id = current.account.id rescue 0
+    @destination = destination
     @current = current
     @nav_classes = nav_classes
     @link_classes = link_classes
