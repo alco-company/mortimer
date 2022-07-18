146162c571a9bca705a25480bfee983c4136e85b
Walther H Diechmann <walther@diechmann.net>
2022-07-18 16:01:36 +0200

    rescue from time_parser

--- lib/time_parser.rb
+++ lib/time_parser.rb
@@ -2,6 +2,10 @@ module TimeParser
 
   # YYMMDD
   def parse_yymmdd val 
-    DateTime.parse "%s/%s/%s" % [ val[0,2],val[2,2],val[4,2] ]
+    begin
+      DateTime.parse "%s/%s/%s" % [ val[0,2],val[2,2],val[4,2] ]
+    rescue => exception
+      false
+    end
   end
 end
