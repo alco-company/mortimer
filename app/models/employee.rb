8d12a20b433da37b6bc20fc7baabccb028fe1426
Walther H Diechmann <walther@diechmann.net>
2022-08-09 17:14:51 +0200

    signup pupils for employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,6 +3,8 @@ class Employee < AbstractResource
 
   has_secure_token :access_token
   has_one_attached :mug_shot
+  has_and_belongs_to_many :pupils
+  accepts_nested_attributes_for :pupils, allow_destroy: true
 
   validates :pin_code, uniqueness: true
   
@@ -12,6 +14,11 @@ class Employee < AbstractResource
   def self.default_scope
   #   where("assetable" deleted_at: nil)
     Employee.all.joins(:asset)
+  end
+
+  def signed_pupils=(ppls)
+    pupils.delete_all
+    ppls.keys.each { |k| pupils << Pupil.find(k) }
   end
 
   #
