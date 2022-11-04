406df801165e5500c57e215a5dc5860702e4504b
Walther H Diechmann <walther@diechmann.net>
2022-11-04 17:15:45 +0100

    tailwind components

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,8 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/components/**/*.*',
+    './app/components/**/*.html.erb',
+    './app/components/**/*.rb',
     './config/locales/*.yml',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
