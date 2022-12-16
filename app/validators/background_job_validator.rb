939d0670a4a8513ac95e9016ccc76151efb7fc4a
Walther H Diechmann <walther@diechmann.net>
2022-12-16 16:43:20 +0100

    rescue error on klass

--- app/validators/background_job_validator.rb
+++ app/validators/background_job_validator.rb
@@ -1,8 +1,14 @@
 class BackgroundJobValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    bool = record.klass.constantize.respond_to? :set
-    unless bool 
-      record.errors.add(attribute, :blank, message: I18n.translate(".klass_not_background_job"))
+    begin 
+      bool = record.klass.constantize.respond_to? :set
+      unless bool 
+        record.errors.add(attribute, :blank, message: I18n.translate(".klass_not_background_job"))
+      end
+    rescue Exception => err
+      record.errors.add(attribute, :missing, message: I18n.translate(".klass_not_background_job"))
     end
+  rescue Exception => err 
+      record.errors.add(attribute, :missing, message: I18n.translate(".error_unknown"))
   end
 end
