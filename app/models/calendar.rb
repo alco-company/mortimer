8e6225bcfa775cde373475c7b9dc03f94de1c7ea
Walther H Diechmann <walther@diechmann.net>
2022-12-23 10:00:08 +0100

    wip calendar entries

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -11,11 +11,12 @@ class Calendar < AbstractResource
   # find all events pertaining to date
   # and punch them
   def punch_work_related_events datetime, owner_asset
-    Event.unscoped.where( calendar_id: id, eventable_type: nil, started_at: (datetime.at_beginning_of_day)..((datetime+1.day).at_beginning_of_day)).each do |e|
-      params = []
-      AssetWorkTransactionService.new.create_automated_employee_punch_transaction owner_asset, params
-      say "punch_work_related_events #{e.id}"
-    end
+    # events.where.not(eventable_type: "AssetWorkTransaction").where(started_at: [..datetime.end_of_day])
+    # Event.unscoped.where( calendar_id: id, eventable_type: nil, started_at: (datetime.at_beginning_of_day)..((datetime+1.day).at_beginning_of_day)).each do |e|
+    #   params = []
+    #   AssetWorkTransactionService.new.create_automated_employee_punch_transaction owner_asset, params
+    #   say "punch_work_related_events #{e.id}"
+    # end
   end
 
 end
