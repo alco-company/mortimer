5aca312576392c810bd26c4fe89d4bc9764bffc2
Walther H Diechmann <walther@diechmann.net>
2023-02-01 11:51:46 +0100

    show background_job active status

--- app/helpers/background_jobs_helper.rb
+++ app/helpers/background_jobs_helper.rb
@@ -3,8 +3,11 @@ module BackgroundJobsHelper
   # show a button if job has an id
   # signifying that it has been scheduled to run
   def delete_job resource, user=nil
-    return "" if resource.job_id.blank?
     I18n.locale = user.profile.locale rescue I18n.default_locale
+    return raw %(
+      <span class="inline-flex items-center rounded-full bg-yellow-100 px-3 py-0.5 text-sm font-medium text-yellow-800">#{ I18n.translate('not_active')}</span>
+    ) unless resource.active
+    return "" if resource.job_id.blank?
     button_to t(".delete_run_job"), delete_run_job_background_job_url(resource), class: "inline-flex items-center rounded-full bg-pink-100 px-3 py-0.5 text-sm font-medium text-pink-800"
   end
 end
