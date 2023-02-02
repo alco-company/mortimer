c85319fa493b54b900a842abad16419fbc230270
Walther H Diechmann <walther@diechmann.net>
2023-02-02 22:14:28 +0100

    restructure and add id's

---
+++ app/components/element/action_button_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Element::ActionButtonComponent < ApplicationComponent
+
+  def initialize(  )
+  end
+
+end
--- app/components/resource/user_profile_component.rb
+++ app/components/resource/user_profile_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Resource::UserProfileComponent < ViewComponent::Base
-  def initialize(current:, container_classes:, labelledby:)
+  def initialize(current:, destination:, container_classes:, labelledby:)
     @current = current
+    @destination = destination
     @container_classes = container_classes
     @labelledby = labelledby
   end
