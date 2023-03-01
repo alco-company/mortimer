47335748c560dc49ef4fea4b56020c1d435370e3
Walther H Diechmann <walther@diechmann.net>
2023-03-01 23:49:40 +0100

    fix regression on profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,21 @@ class ProfilesController < SpeicherController
     @resource
   end
 
+  # The very essense of U in the CRUD - 
+  # any controller not required to do special stuff
+  # will inherit from this method
+  # update_resource will instantiate an AbstractResourceService object
+  # and call update on it, returning an instance of Result (defined on AbstractResourceService)
+  def update_resource
+    # Each resource could have it's own - 
+    result = "#{resource_class.to_s}Service".constantize.new.update  resource(), resource_params
+    resource= result.record
+    case result.status
+    when :updated; head 200
+    when :not_valid; render turbo_stream: turbo_stream.replace( resource_form, partial: 'form' ), status: :unprocessable_entity
+    end    
+  end
+
   private
 
     # Only allow a list of trusted parameters through.
