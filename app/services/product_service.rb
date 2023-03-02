bda7df3bba954b22193864279c09a31ef9c3d0f0
Walther H Diechmann <walther@diechmann.net>
2023-03-02 12:36:23 +0100

    push methods off participant and product

--- app/services/product_service.rb
+++ app/services/product_service.rb
@@ -9,23 +9,23 @@ class ProductService < AssetService
     Asset.new account: account, assetable: prod
   end
 
-  def create_product s, parm 
-    acc = Asset.unscoped.where(assetable: s).first.account
-    sup = SupplierService.new.get_by :supplier_barcode, acc, parm
-    prod = Product.new( supplier: sup, supplier_barcode: parm["ean14"] )
-    resource = Asset.new account: acc, name: parm["ean14"], assetable: prod
-    result = create(resource)
-    return result.record if result.created?
-    nil
-  end
+  # def create_product s, parm 
+  #   acc = Asset.unscoped.where(assetable: s).first.account
+  #   sup = SupplierService.new.get_by :supplier_barcode, acc, parm
+  #   prod = Product.new( supplier: sup, supplier_barcode: parm["ean14"] )
+  #   resource = Asset.new account: acc, name: parm["ean14"], assetable: prod
+  #   result = create(resource)
+  #   return result.record if result.created?
+  #   nil
+  # end
 
-  def update resource, resource_params
-    result = super(resource, resource_params)
-    if result.updated?
-      resource.assetable.stocked_products.each{ |r| r.asset.update name: resource.name } if resource.assetable and resource.assetable.stocked_products.any?
-    end
-    result
-  end
+  # def update resource, resource_params
+  #   result = super(resource, resource_params)
+  #   if result.updated?
+  #     resource.assetable.stocked_products.each{ |r| r.asset.update name: resource.name } if resource.assetable and resource.assetable.stocked_products.any?
+  #   end
+  #   result
+  # end
   
   def get_stocked_product s, sl, prod, parm
     return nil if prod.nil? or prod.assetable.nil?
