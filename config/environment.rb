2e3cecb0db735146ecab8b0fc2a3703b5b612798
Walther H Diechmann <walther@diechmann.net>
2023-03-03 17:23:17 +0100

    configure email through SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,15 @@ require_relative "application"
 oauth_environment_variables = File.join(Rails.root, 'config', 'oauth_environment_variables.rb')
 load(oauth_environment_variables) if File.exist?(oauth_environment_variables)
 
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+  :password => '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', # This is the secret sendgrid API key which was issued during API key creation
+  :domain => 'alco.dk',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!
