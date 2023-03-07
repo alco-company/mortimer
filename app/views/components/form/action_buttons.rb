2fbaed43a077045578fd508e17f38480e96d1613
Walther H Diechmann <walther@diechmann.net>
2023-03-07 22:34:07 +0100

    fixing misaligned action buttons

--- app/views/components/form/action_buttons.rb
+++ app/views/components/form/action_buttons.rb
@@ -12,7 +12,7 @@ module Views
     end
 
     def template()
-      div( class: "sticky bottom-0 flex justify-start px-4 border-t border-gray-200 bg-gray-50 py-5 sm:px-6 z-10") do
+      div( class: "fixed w-screen max-w-md bottom-0 w-2/3 flex justify-start px-4 border-t border-gray-200 bg-gray-50 py-5 sm:px-6 z-10") do
         div( class: "flex-none w-1/3 h-14") do
           if @deleteable
             link_to( @delete_url, class: "button delete-button", data: { turbo_confirm: 'Are you sure?', turbo_method: :delete, action: "click->form-sleeve#toggle" }) { helpers.t :delete} 
